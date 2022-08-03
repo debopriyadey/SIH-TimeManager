95c5e71692a7ada40110f175d8c646c8b0a19407
Gourav <you@example.com>
2022-08-03 23:43:33 +0530

    onboarding screen

--- react-native/App.js
+++ react-native/App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Onboarding
+  pages={[
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('./assets/favicon.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    },
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('./assets/adaptive-icon.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    },
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('./assets/icon.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    }
+  ]}
+/>
     </View>
   );
 }
