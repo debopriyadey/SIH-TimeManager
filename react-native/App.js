3e77ea7f141ebe8bc0ddb72988fe48f6ddeaa649
Gourav <you@example.com>
2022-08-04 20:15:31 +0530

    error need to resolve

--- react-native/App.js
+++ react-native/App.js
@@ -5,13 +5,128 @@ import {createStackNavigator} from '@react-navigation/stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import HomeScreen from './screens/HomeScreen';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import MainTabScreen from './screens/MainTabScreen';
+import SupportScreen from './screens/SupportScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import BookmarkScreen from './screens/BookmarkScreen';
+import { AuthContext } from './components/context';
+import RootStackScreen from './screens/RootStackScreen';
+
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
+
+  const CustomDefaultTheme = {
+    ...NavigationDefaultTheme,
+    ...PaperDefaultTheme,
+    colors: {
+      ...NavigationDefaultTheme.colors,
+      ...PaperDefaultTheme.colors,
+      background: '#ffffff',
+      text: '#333333'
+    }
+  }
+
+  const CustomDarkTheme = {
+    ...NavigationDarkTheme,
+    ...PaperDarkTheme,
+    colors: {
+      ...NavigationDarkTheme.colors,
+      ...PaperDarkTheme.colors,
+      background: '#333333',
+      text: '#ffffff'
+    }
+  }
+
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'APP_LOADED':
+        return {
+          ...prevState,
+          isLoading: false,
+        };
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+        };
+    }
+  };
+
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  
+  const authContext = React.useMemo(() => ({
+    signIn: async(foundUser) => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+      const userToken = String(foundUser[0].userToken);
+      const userName = foundUser[0].username;
+      
+      try {
+        await AsyncStorage.setItem('userToken', userToken);
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'LOGIN', id: userName, token: userToken });
+    },
+    signOut: async() => {
+      // setUserToken(null);
+      // setIsLoading(false);
+      try {
+        await AsyncStorage.removeItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      dispatch({ type: 'LOGOUT' });
+    },
+    signUp: () => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+    },
+    toggleTheme: () => {
+      setIsDarkTheme( isDarkTheme => !isDarkTheme );
+    }
+  }), []);
 
   useEffect(() => {
     async function setData () {
+      let userToken;
+      userToken = null;
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'RETRIEVE_TOKEN', token: userToken });
+
       const appData = await AsyncStorage.getItem('isAppFirstLaunched');
       if (appData === null) {
         await AsyncStorage.setItem('isAppFirstLaunched', 'true');
@@ -25,32 +140,26 @@ const App = () => {
     setData()
   }, [])
 
-  // React.useEffect(async () => {
-  //   const appData = await AsyncStorage.getItem('isAppFirstLaunched');
-  //   if (appData == null) {
-  //     setIsAppFirstLaunched(true);
-  //     AsyncStorage.setItem('isAppFirstLaunched', 'false');
-  //   } else {
-  //     setIsAppFirstLaunched(false);
-  //   }
-
-  //   // AsyncStorage.removeItem('isAppFirstLaunched');
-  // }, []);
-  
+ 
   return (
-    isAppFirstLaunched != null && (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          {isAppFirstLaunched && (
-            <Stack.Screen
-              name="OnboardingScreen"
-              component={OnboardingScreen}
-            />
-          )}
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        </Stack.Navigator>
+    <PaperProvider theme={theme}>
+        <AuthContext.Provider value={authContext}>
+      <NavigationContainer  theme={theme}>
+      { loginState.userToken !== null ? (
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+          <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+          <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+          <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+        </Drawer.Navigator>
+      )
+    :
+      <RootStackScreen/>
+    }
+
       </NavigationContainer>
-    )
+      </AuthContext.Provider>
+      </PaperProvider>
   );
 };
 
---
+++ react-native/screens/RootStackScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OnboardingScreen from './OnboardingScreen';
+import SignInScreen from './SignInScreen';
+import SignUpScreen from './SignUpScreen';
+
+const RootStack = createStackNavigator();
+
+const RootStackScreen = ({navigation}) => (
+    <RootStack.Navigator headerMode='none'>
+        <RootStack.Screen name="OnboardingScreen" component={OnboardingScreen}/>
+        <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
+        <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
+    </RootStack.Navigator>
+);
+
+export default RootStackScreen;
