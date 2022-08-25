5baf178abc97dc9d16b97fc9ca9ed0841f0012f5
am-here <harshbaid.ind@gmail.com>
2022-08-25 17:37:06 +0530

    recieving notification tokens

--- react-native/components/Notifications/AppNotifications.js
+++ react-native/components/Notifications/AppNotifications.js
@@ -1,18 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   Text,
   View,
   StyleSheet,
   TouchableWithoutFeedback,
   FlatList,
+  Button,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
 import notifications from "../../model/Notifications";
 import NotificationCard from "./NotificationCard";
 import DeleteComp from "./../Common/DeleteComp";
 
 function AppNotifications({ navigation }) {
   const [messages, setMessages] = useState(notifications);
+  const [token, setToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    return token;
+  }
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id));
   };
@@ -21,6 +59,27 @@ function AppNotifications({ navigation }) {
       navigation.navigate("Schedule");
     }
   };
+
+  useEffect(() => {
+    console.log("component did mount");
+    registerForPushNotificationsAsync()
+      .then((data) => {
+        console.log(data);
+        setToken(data);
+      })
+      .catch((err) => console.log(err));
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+        console.log(notification);
+      });
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      // Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.headerView}>
